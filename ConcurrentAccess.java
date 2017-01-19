61dbb10eedf54a7dd8d7826f68e2748e1a3c491d
Antonio Pierro <antonio.pierro@gmail.com>
2017-01-19 23:21:58 +0100

    Create ConcurrentAccess.java

---
+++ ConcurrentAccess.java
@@ -0,0 +1,45 @@
+/*
+ * Concurrent access to shared variables
+ */
+package concurrent;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Antonio Pierro
+ */
+
+class MyThread implements Runnable {
+    
+    @Override
+    public void run(){
+       for(int i = 0; i < 5000 ; i++) {
+           incrementCounter();
+       }  
+    }
+    public static void incrementCounter(){
+        ConcurrentAccess.counter++;   
+    }
+}
+
+public class ConcurrentAccess {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int counter = 0;
+    
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyThread());
+        thread1.start();
+        Thread thread2 = new Thread(new MyThread());
+        thread2.start();
+        
+        while(thread1.isAlive()||thread2.isAlive()){}
+          
+        System.out.println("Counter : " + counter);
+        
+    }
+}

794928b1bc3f781e5075108c8ab4fc6a6c37cbfb
Antonio Pierro <antonio.pierro@gmail.com>
2017-03-16 21:47:27 +0100

    Added comment about daemon thread.

--- DaemonThread.java
+++ DaemonThread.java
@@ -1,3 +1,10 @@
+/**
+ * There are two types of threads user thread and daemon thread. 
+ * The daemon thread is a service provider thread. 
+ * It provides services to the user thread. 
+ * Its life depends on the user threads i.e. when all the user threads dies, JVM termintates daemon threads automatically.
+**/
+
 public class DaemonThread {
     
     public static void main(String[] args) throws InterruptedException {
